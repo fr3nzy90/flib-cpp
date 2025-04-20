8d99be9bd9325c7a40ce20a3d5a56777da28f76b
Luka Arnečič <luka.arnecic@outlook.com>
2025-04-20 22:32:02 +0200

    Minor commit - fixed build warnings resulting in errors due to catch2

--- 3rd-party/catch2/include/catch2/catch2.hpp
+++ 3rd-party/catch2/include/catch2/catch2.hpp
@@ -5,11 +5,11 @@
 
 // Need to disable certain compiler warnings caused in 3rd-party header
 #ifdef _MSC_VER
-#  pragma warning(disable: 4003)
+#  pragma warning(disable: 4003 4324)
 #endif
 
 #include "../3rd-party/v3.8.1/catch_amalgamated.hpp"
 
 #ifdef _MSC_VER
-#  pragma warning(default: 4003)
+#  pragma warning(default: 4003 4324)
 #endif
